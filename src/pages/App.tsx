774cc3d900578d81997ecae676e908bd9f97ef91
Jason Zhao <jason1382311@gmail.com>
2023-03-15 00:25:00 -0700

    add firebase config

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,22 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./main-page/mainPage";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCT91Qc_y9wARXQBTFfWljMfGXlQyTzmU8",
+
+  authDomain: "ai-planner-abaca.firebaseapp.com",
+
+  projectId: "ai-planner-abaca",
+
+  storageBucket: "ai-planner-abaca.appspot.com",
+
+  messagingSenderId: "605428409082",
+
+  appId: "1:605428409082:web:3ddab5e25a2e07805edf43",
+
+  measurementId: "G-9675QBEZDN",
+};
+
 function App() {
   return (
     <>
