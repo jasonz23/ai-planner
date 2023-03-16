910d69b3c28a0bee2c7f353de2744d1211999ec3
Jason Zhao <jason1382311@gmail.com>
2023-03-16 00:17:42 -0700

    add schedular and tasks/events

--- src/pages/calendar/calendar.tsx
+++ src/pages/calendar/calendar.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import NavBar from "../../components/nav-bar/navBar";
+import { Scheduler } from "../../components/scheduler";
+import { minWidth } from "../../constants/dimensions";
+import { useAppSelector } from "../../slices";
 
-const CalendarPage = () => {};
+const CalendarPage = () => {
+  const tasks = useAppSelector((state) => state?.tasks?.tasks);
+  return (
+    <div>
+      <NavBar />
+      <div
+        style={{
+          marginTop: "70px",
+          marginLeft: window.innerWidth <= minWidth ? "" : "88px",
+        }}
+      >
+        <Scheduler events={tasks} view="month" />
+      </div>
+    </div>
+  );
+};
 
 export default CalendarPage;
