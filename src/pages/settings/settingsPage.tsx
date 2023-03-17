06a0251b65d0488eafa31f05fe905ba27a96222b
Jason Zhao <jason1382311@gmail.com>
2023-03-16 22:04:11 -0700

    fix styling for settings and tasks page

--- src/pages/settings/settingsPage.tsx
+++ src/pages/settings/settingsPage.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import NavBar from "../../components/nav-bar/navBar";
+import { minWidth } from "../../constants/dimensions";
 
 const SettingsPage = () => {
   return (
     <div>
       <NavBar />
-      <div>Under Development</div>
+      <div
+        style={{
+          marginTop: "70px",
+          marginLeft: window.innerWidth <= minWidth ? "" : "88px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        Under Development
+      </div>
     </div>
   );
 };
--- src/pages/tasks-page/tasksPage.tsx
+++ src/pages/tasks-page/tasksPage.tsx
@@ -1,10 +1,20 @@
 import react from "react";
 import NavBar from "../../components/nav-bar/navBar";
+import { minWidth } from "../../constants/dimensions";
 const tasksPage = () => {
   return (
     <div>
       <NavBar />
-      <div>Under Development</div>
+      <div
+        style={{
+          marginTop: "70px",
+          marginLeft: window.innerWidth <= minWidth ? "" : "88px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        Under Development
+      </div>
     </div>
   );
 };
