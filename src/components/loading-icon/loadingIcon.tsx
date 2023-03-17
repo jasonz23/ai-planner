f725004155ab5018602dbc053a98f2a141cf121d
Jason Zhao <jason1382311@gmail.com>
2023-03-16 21:53:21 -0700

    removed loading text

--- src/components/loading-icon/loadingIcon.tsx
+++ src/components/loading-icon/loadingIcon.tsx
@@ -4,7 +4,6 @@ import "./loadingIcon.css";
 const LoadingIcon = () => {
   return (
     <div>
-      <p style={{ color: "black", fontSize: "20px" }}>Loading ...</p>
       <div className="lds-roller">
         <div></div>
         <div></div>
