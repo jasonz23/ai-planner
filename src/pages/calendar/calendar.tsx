2303867ef5a7ba8235211925d6685ca986530530
Jason Zhao <jason1382311@gmail.com>
2023-03-26 15:11:08 -0700

    redirect user if not signed in and remove navigator

--- src/pages/calendar/calendar.tsx
+++ src/pages/calendar/calendar.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import NavBar from "../../components/nav-bar/navBar";
 import { Scheduler } from "../../components/scheduler";
 import { minWidth } from "../../constants/dimensions";
 import { useAppSelector } from "../../slices";
+import { auth } from "../App";
 
 const CalendarPage = () => {
+  const navigate = useNavigate();
   const tasks = useAppSelector((state) => state?.tasks?.tasks);
+
+  useEffect(() => {
+    if (!auth.currentUser) {
+      navigate("/");
+    }
+  });
   return (
     <div>
       <NavBar />
@@ -25,6 +34,7 @@ const CalendarPage = () => {
             startHour: 0,
             endHour: 23,
           }}
+          disableViewNavigator={true}
         />
       </div>
     </div>
