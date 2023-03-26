5dc2804265d046ea6d072f6ef3d98e0dc01753f7
Jason Zhao <jason1382311@gmail.com>
2023-03-26 15:11:20 -0700

    implment tasks page to show tasks

--- src/pages/tasks-page/tasksPage.tsx
+++ src/pages/tasks-page/tasksPage.tsx
@@ -1,7 +1,25 @@
-import react from "react";
+import react, { useEffect, useState } from "react";
 import NavBar from "../../components/nav-bar/navBar";
+import Task from "../../components/task/task";
 import { minWidth } from "../../constants/dimensions";
-const tasksPage = () => {
+import { useAppSelector } from "../../slices";
+import { filterObjectsByEndTime } from "../../utils/helpers";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../App";
+import { ToastContainer } from "react-toastify";
+
+const TasksPage = () => {
+  const tasks = useAppSelector((state) => state?.tasks?.tasks);
+  const [todayTasks, setTodayTasks] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTodayTasks(filterObjectsByEndTime(tasks));
+    if (!auth.currentUser) {
+      navigate("/");
+    }
+  }, [tasks]);
+
   return (
     <div>
       <NavBar />
@@ -11,12 +29,32 @@ const tasksPage = () => {
           marginLeft: window.innerWidth <= minWidth ? "" : "88px",
           display: "flex",
           justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "20px",
         }}
       >
-        Under Development
+        <div
+          style={{ marginTop: "20px", fontWeight: "bold", fontSize: "25px" }}
+        >
+          Todays Tasks
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "20px",
+            width: "100%",
+          }}
+        >
+          {todayTasks.map((task: any, index: number) => {
+            return <Task index={index} task={task} key={index} />;
+          })}
+        </div>
       </div>
     </div>
   );
 };
 
-export default tasksPage;
+export default TasksPage;
